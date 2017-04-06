c63a821c667e6897294bc255d28122559f38b1bf
hassey13 <ehasselbring@gmail.com>
2017-04-06 17:38:03 -0400

    new styles for min/ close buttons

--- src/components/Messaging/MessageHeader.js
+++ src/components/Messaging/MessageHeader.js
@@ -6,7 +6,16 @@ export default class MessageHeader extends React.Component {
 
     return (
       <div className='message-header active'>
-        { this.props.recipient.name }
+        <div className='message-header-text'>
+          { this.props.recipient.name }
+        </div>
+        <div className='message-header-min hover'>
+          -
+        </div>
+        <div className='message-header-close hover'>
+          x
+        </div>
+
       </div>
     )
   }
